302696e1eed2fe17e75133e6f683a5407699f190
Reza Mousavian <rezamosavian1014@gmail.com>
2020-07-16 01:16:01 -0700

    added appbar with sliverAppbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -7,7 +8,45 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            forceElevated: true,
+            floating: true,
+            snap: true,
+            pinned: true,
+            expandedHeight: 120.0,
+//            leading option
+            leading: IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+              iconSize: 30.0,
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.add),
+                iconSize: 30.0,
+                onPressed: () {},
+              )
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text("simplebudget"),
+            ),
+          ),
+          SliverList(
+            delegate:
+                SliverChildBuilderDelegate((BuildContext context, int index) {
+              return Container(
+                margin: EdgeInsets.all(10.0),
+                height: 100.0,
+                color: Colors.red,
+              );
+            }, childCount: 10),
+          ),
+        ],
+      ),
+    );
   }
 }
-
